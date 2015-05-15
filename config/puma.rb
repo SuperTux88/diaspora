150b316037ebca1714e6c0d9c9b254883d80ec55
Benjamin Neff <benjamin@coding4coffee.ch>
2015-05-15 22:00:49 +0200

    switch from unicorn to puma

---
+++ config/puma.rb
@@ -0,0 +1,16 @@
+require File.expand_path('../load_config', __FILE__)
+
+daemonize
+
+state_path '/var/run/diaspora/diaspora.state'
+pidfile '/var/run/diaspora/diaspora.pid'
+bind 'unix:///var/run/diaspora/diaspora.sock'
+
+worker_timeout AppConfig.server.unicorn_timeout.to_i
+
+stdout_redirect AppConfig.server.stdout_log.present? ? AppConfig.server.stdout_log.get : '/dev/null',
+                AppConfig.server.stderr_log.present? ? AppConfig.server.stderr_log.get : '/dev/null'
+
+workers 2
+threads 4, 16
+preload_app!
