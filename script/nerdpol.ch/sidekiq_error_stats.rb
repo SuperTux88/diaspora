68cac2685c02ca692e20189822f3989c26134d3c
Benjamin Neff <benjamin@coding4coffee.ch>
2016-08-21 03:12:09 +0200

    improve sidekiq_error_stats.rb

--- script/nerdpol.ch/sidekiq_error_stats.rb
+++ script/nerdpol.ch/sidekiq_error_stats.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 
-ENV['RAILS_ENV'] ||= "production"
+ENV["RAILS_ENV"] ||= "production"
 require_relative "../../config/environment"
 require "terminal-table"
 
 def count_errors(set)
-   set.map { |job| job.item["error_class"] }.each_with_object(Hash.new(0)) { |error, counts| counts[error] += 1 }.sort_by { |error, count| count }.reverse
+  set.map {|job| job.item["error_class"] }
+     .each_with_object(Hash.new(0)) {|error, counts| counts[error] += 1 }
+     .sort_by {|_, count| count }.reverse
 end
 
 def dump_table(title, set)
@@ -13,5 +15,24 @@ def dump_table(title, set)
   puts Terminal::Table.new title: title, headings: %w(Error Count), rows: set
 end
 
-dump_table "Retries", count_errors(Sidekiq::RetrySet.new)
-dump_table "Dead", count_errors(Sidekiq::DeadSet.new)
+def dump_error_details(title, set, error_class)
+  puts
+  puts "Set: #{title} | Error: #{error_class}"
+  set.select {|job| job.item["error_class"] == error_class }
+     .each do |job|
+    puts "================================================="
+    puts "jid: #{job.item['jid']}"
+    puts "error_message: #{job.item['error_message']}"
+    puts "error_backtrace:\n  #{job.item['error_backtrace'].join("\n  ")}"
+  end
+end
+
+if ARGV.empty?
+  dump_table("Retries", count_errors(Sidekiq::RetrySet.new))
+  dump_table("Dead", count_errors(Sidekiq::DeadSet.new))
+elsif ARGV.length == 1
+  dump_error_details("Retries", Sidekiq::RetrySet.new, ARGV[0])
+  dump_error_details("Dead", Sidekiq::DeadSet.new, ARGV[0])
+else
+  puts "Usage: sidekiq_error_stats.rb [error_class]"
+end
