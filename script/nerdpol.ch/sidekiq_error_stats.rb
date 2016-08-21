fba4ae8e8baf0ffa9f328f683a1c29d08ab138da
Benjamin Neff <benjamin@coding4coffee.ch>
2016-08-21 03:31:49 +0200

    print details for a job

--- script/nerdpol.ch/sidekiq_error_stats.rb
+++ script/nerdpol.ch/sidekiq_error_stats.rb
@@ -27,12 +27,31 @@ def dump_error_details(title, set, error_class)
   end
 end
 
+def dump_jid_details(set, jid)
+  set.find {|job| job.item["jid"] == jid }
+     .try do |job|
+    puts "jid: #{job.item['jid']}"
+    puts "class: #{job.item['class']}"
+    puts "args:"
+    pp job.item["args"]
+    puts "error_class: #{job.item['error_class']}"
+    puts "error_message: #{job.item['error_message']}"
+    puts "error_backtrace:\n  #{job.item['error_backtrace'].join("\n  ")}"
+  end
+end
+
 if ARGV.empty?
   dump_table("Retries", count_errors(Sidekiq::RetrySet.new))
   dump_table("Dead", count_errors(Sidekiq::DeadSet.new))
-elsif ARGV.length == 1
-  dump_error_details("Retries", Sidekiq::RetrySet.new, ARGV[0])
-  dump_error_details("Dead", Sidekiq::DeadSet.new, ARGV[0])
+elsif ARGV.length == 2 && ARGV[0] == "--error_class"
+  dump_error_details("Retries", Sidekiq::RetrySet.new, ARGV[1])
+  dump_error_details("Dead", Sidekiq::DeadSet.new, ARGV[1])
+elsif ARGV.length == 2 && ARGV[0] == "--jid"
+  dump_jid_details(Sidekiq::RetrySet.new, ARGV[1])
+  dump_jid_details(Sidekiq::DeadSet.new, ARGV[1])
 else
-  puts "Usage: sidekiq_error_stats.rb [error_class]"
+  puts "Usage:"
+  puts "Statistics: sidekiq_error_stats.rb"
+  puts "Error class details: sidekiq_error_stats.rb --error_class ErrorClass"
+  puts "Print jid details: sidekiq_error_stats.rb --jid jid"
 end
