3a6ef912cb8382eaf25a93e6a538429335bd1e1f
Benjamin Neff <benjamin@coding4coffee.ch>
2017-01-03 02:56:50 +0100

    Update diaspora_spam.rb

--- script/nerdpol.ch/diaspora_spam.rb
+++ script/nerdpol.ch/diaspora_spam.rb
@@ -5,31 +5,34 @@ spam_accounts = %w(spamacc@podA spamacc@podB spamacc@mypod)
 
 # Delete comments even if spammer isn't a local user or spam isn't on a
 # local users account.
-# And delete posts of users in spam_accounts that aren't local.
+# And close accounts of users in spam_accounts that aren't local.
 always_delete = true
 
 # Keep empty (%w() or []) to retract spam comments from remote accounts
 # for all local users
-retract_for = %w(userA userB)
+# retract_for = %w(userA userB)
+retract_for = []
 
 ###########################################################################
 
 # Load diaspora environment
-ENV['RAILS_ENV'] ||= "production"
-require_relative 'config/environment'
+ENV["RAILS_ENV"] ||= "production"
+require_relative "../../config/environment"
 
 local_spammers, remote_spammers = Person.where(diaspora_handle: spam_accounts).partition(&:local?)
 
 # Retract all comments of local spammers and close their accounts
 local_spammers.each do |spammer|
   Comment.where(author_id: spammer.id).each do |comment|
+    puts "delete #{comment.guid} from post #{comment.parent.guid}"
     spammer.owner.retract(comment)
-   end
-   spammer.owner.close_account!
+  end
+  spammer.owner.close_account!
 end
 
 # Retract all spam comments on posts of local users and delete the rest
 Comment.where(author_id: remote_spammers.map(&:id)).each do |comment|
+  puts "delete #{comment.guid} from post #{comment.parent.guid}"
   post_author = comment.parent.author
   if post_author.local? && (retract_for.include?(post_author.owner.username) || retract_for.empty?)
     post_author.owner.retract(comment)
@@ -38,7 +41,5 @@ Comment.where(author_id: remote_spammers.map(&:id)).each do |comment|
   end
 end
 
-# Destroy posts of remote users if wanted
-if always_delete
-  Post.where(author_id: remote_spammers.map(&:id)).each(&:destroy)
-end
+# Close accounts of remote users if wanted
+remote_spammers.each {|spammer| AccountDeleter.new(spammer.diaspora_handle).perform! } if always_delete
